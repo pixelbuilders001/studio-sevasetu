8d77a678e638ec7ed195e72af28a67ccb6aeb54c
Rajeev Sharma <freelancer.rajeev61@gmail.com>
2025-12-29 14:22:21 +0000

    https://images.pexels.com/photos/4981799/pexels-photo-4981799.jpeg put t

--- next.config.ts
+++ next.config.ts
@@ -28,8 +28,16 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
 
 export default nextConfig;
+
+    
