ef85f927355d7a55fd64b83a4520867f4d59289f
Rajeev Sharma <freelancer.rajeev61@gmail.com>
2025-12-29 17:25:19 +0000

    remove paytm image

--- src/components/PaymentIcons.tsx
+++ src/components/PaymentIcons.tsx
@@ -13,7 +13,6 @@ export default function PaymentIcons() {
         <span className="font-semibold text-sm text-muted-foreground pl-1">COD</span>
         <div className="w-px h-5 bg-border"></div>
         <div className="flex items-center gap-4">
-             <Image src="https://upload.wikimedia.org/wikipedia/commons/4/42/Paytm_logo.svg" alt="Paytm" width={55} height={18} />
              <Image src="https://upload.wikimedia.org/wikipedia/commons/7/71/PhonePe_Logo.svg" alt="PhonePe" width={80} height={20} />
              <Image src="https://upload.wikimedia.org/wikipedia/commons/f/f2/Google_Pay_Logo.svg" alt="Google Pay" width={50} height={20} />
         </div>
